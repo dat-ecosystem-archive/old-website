0dc0abd3dd2893c667e759cdef2de42a229344b5
Karissa McKelvey <krmckelv@gmail.com>
2015-12-04 00:15:40 -0500

    add blog style, copy old posts from usopendata.org

--- posts.js
+++ posts.js
@@ -1,14 +1,30 @@
 module.exports = [
   {
-    date: "2015-10-01 23:10",
-    title: 'This is a test',
-    teaser: 'I am a tease',
-    name: 'test'
+    date: '2015-07-29 23:10',
+    title: 'Dat goes beta!',
+    author: 'Karissa McKelvey',
+    teaser: 'After a long year of alpha testing, dat goes beta.',
+    name: '2015-07-29-dat-beta'
   },
   {
-    date: "2015-10-01 23:10",
-    title: 'This is a test2',
-    teaser: 'I am a tease2',
-    name: 'test2'
+    date: '2015-04-03 23:10',
+    title: 'Sloan Redoubles Dat Funding',
+    author: 'Waldo Jacquith',
+    teaser: 'We are thrilled to announce a $640,000 grant from the Sloan foundation',
+    name: '2015-04-03-sloan'
+  },
+  {
+    date: '2014-08-19 23:10',
+    title: 'Announcing the dat Alpha',
+    author: 'Max Ogden',
+    teaser: 'We are really excited to release the first major version of dat along with a new website.',
+    name: '2014-08-19-dat-alpha'
+  },
+  {
+    date: '2014-04-02 23:10',
+    title: 'Sloan funding dat development',
+    author: 'Waldo Jacquith',
+    teaser: 'Thanks to Josh Greenberg and the Sloan Foundation for their generosity.',
+    name: '2014-04-02-dat'
   }
 ]
