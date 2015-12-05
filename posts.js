b91d8a25b87d1888f678934b3b530a873eb1b4ef
Karissa McKelvey <krmckelv@gmail.com>
2015-12-05 01:58:15 -0500

    add draft for 1.0 design post

--- posts.js
+++ posts.js
@@ -1,27 +1,34 @@
 module.exports = [
+  // {
+  //   date: '2015-12-05',
+  //   title: 'Designing Dat 1.0',
+  //   author: 'Karissa McKelvey',
+  //   teaser: 'Defining the design decisions around Dats new 1.0',
+  //   name: '2015-12-05-1.0-design'
+  // },
   {
-    date: '2015-07-29 23:10',
+    date: '2015-07-29',
     title: 'Dat goes beta!',
     author: 'Karissa McKelvey',
     teaser: 'After a long year of alpha testing, dat goes beta.',
     name: '2015-07-29-dat-beta'
   },
   {
-    date: '2015-04-03 23:10',
+    date: '2015-04-03',
     title: 'Sloan Redoubles Dat Funding',
     author: 'Waldo Jacquith',
     teaser: 'Announcing a generous $640,000 grant from the Sloan foundation',
     name: '2015-04-03-sloan'
   },
   {
-    date: '2014-08-19 23:10',
+    date: '2014-08-19',
     title: 'Announcing the dat Alpha',
     author: 'Max Ogden',
     teaser: 'The first major version of dat along with a new website.',
     name: '2014-08-19-dat-alpha'
   },
   {
-    date: '2014-04-02 23:10',
+    date: '2014-04-02',
     title: 'Sloan funding dat development',
     author: 'Waldo Jacquith',
     teaser: 'Announcing Sloans support of the dat project',
