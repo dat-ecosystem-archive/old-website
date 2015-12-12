8ee295af6b3b89df2b449141c42e41e41faf7a41
Max Ogden <max@maxogden.com>
2015-12-11 17:53:07 -0800

    everything should work now

--- render.js
+++ render.js
@@ -36,6 +36,8 @@ posts.forEach(function (post) {
   var content = marked(fs.readFileSync('posts/' + post.name + '.md').toString())
   var rendered = Handlebars.compile(templates.blog)({posts: posts, content: content, post: post})
   dom('#content').html(rendered)
+  dom('#' + post.name).addClass('active')
+  dom('a[href="/blog"]').addClass('active')
   fs.writeFileSync(outDir + '/blog/' + post.name + '.html', dom.html())
   if (post.date && !renderedFrontPost) {
     renderedFrontPost = true
@@ -43,10 +45,10 @@ posts.forEach(function (post) {
   }
 })
 
-renderStatic(templates.splash, 'index.html')
-renderStatic(templates.about, 'about.html')
-renderStatic(templates.team, 'team.html')
-renderStatic(templates.docs, 'docs.html')
+renderStatic(templates.splash, 'index')
+renderStatic(templates.about, 'about')
+renderStatic(templates.team, 'team')
+renderStatic(templates.docs, 'docs')
 
 // copy static stuff
 cptar('./static', outDir + '/static', function (err) {
@@ -56,76 +58,6 @@ cptar('./static', outDir + '/static', function (err) {
 function renderStatic (template, target) {
   var dom = cheerio.load(templates.index)
   dom('#content').html(template)
-  fs.writeFileSync(outDir + '/' + target, dom.html())
+  dom('a[href="/' + target + '"]').addClass('active')
+  fs.writeFileSync(outDir + '/' + target + '.html', dom.html())
 }
-
-// var routes = [
-//   {
-//     url: '/',
-//     template: fs.readFileSync('./templates/splash.html').toString(),
-//     onrender: function (params) {
-//       $('.tabs a').click(function (e) {
-//         e.preventDefault()
-//         $(this).tab('show')
-//       })
-//       hljs.initHighlightingOnLoad()
-//     }
-//   },
-//   {
-//     url: '/blog',
-//     template: fs.readFileSync('./templates/blog.html').toString(),
-//     data: function (params, cb) {
-//       cb({posts: posts})
-//     },
-//     onrender: function (data) {
-//       $('a.load-document')[0].click()
-//       switchNav('/blog')
-//     }
-//   },
-//   {
-//     url: '/blog/:name',
-//     template: fs.readFileSync('./templates/blog.html').toString(),
-//     data: function (params, cb) {
-//       cb({posts: posts})
-//     },
-//     onrender: function (data) {
-//       xhr('/posts/' + data.name + '.md', function (err, res, data) {
-//         $('#post').html(marked(data))
-//       })
-//       $('#' + data.name).addClass('active')
-//       switchNav('/blog')
-//     }
-//   },
-//   {
-//     url: '/about',
-//     template: fs.readFileSync('./templates/about.html').toString(),
-//     onrender: function (data) { switchNav('/about') }
-//   },
-//   {
-//     url: '/docs',
-//     template: fs.readFileSync('./templates/docs.html').toString(),
-//     data: function (params, cb) {
-//       var url = 'https://raw.githubusercontent.com/maxogden/dat/master/readme.md'
-//       xhr(url, function (err, res, data) {
-//         var readme = marked(data)
-//         return cb({readme: readme})
-//       })
-//     },
-//     onrender: function (data) { switchNav('/docs') }
-//   },
-//   {
-//     url: '/team',
-//     template: fs.readFileSync('./templates/team.html').toString(),
-//     onrender: function (params) {
-//       var gravatar = require('gravatar')
-//       var peeps = document.querySelectorAll('.content-card-small-avatar')
-//       for (var i = 0; i < peeps.length; i++) {
-//         var peep = peeps[i]
-//         var username = peep.getAttribute('data-user')
-//         if (!username) continue
-//         peep.setAttribute('style', "background-image: url('https://github.com/" + username + ".png')")
-//       }
-//       switchNav('/team')
-//     }
-//   }
-// ]
