4f612f28db9b076b32c2e5bccd1682222a2fdfb1
Max Ogden <max@maxogden.com>
2016-01-19 12:24:07 -0800

    publish history post

--- posts.js
+++ posts.js
@@ -2,7 +2,7 @@ module.exports = [
   {
     title: 'Designing Dat 1.0',
     author: 'Karissa McKelvey',
-    teaser: 'Defining the design decisions around Dats new 1.0',
+    teaser: 'Defining the design decisions around the new Dat 1.0',
     name: '2015-12-05-1-design'
   },
   {
@@ -10,6 +10,13 @@ module.exports = [
     author: 'Karissa McKelvey',
     teaser: 'The beginning of the end -- and what it means for science.',
     name: '2015-12-01-proprietary'
+  },
+  {
+    date: '2016-01-19',
+    title: 'A Brief History of Dat',
+    author: 'Max Ogden',
+    teaser: 'History of Dat and the design choices over the last 2 years',
+    name: '2016-01-19-brief-history-of-dat'
   },
   {
     date: '2015-07-29',
@@ -27,14 +34,14 @@ module.exports = [
   },
   {
     date: '2014-08-19',
-    title: 'Announcing the dat Alpha',
+    title: 'Announcing the Dat Alpha',
     author: 'Max Ogden',
     teaser: 'The first major version of dat along with a new website.',
     name: '2014-08-19-dat-alpha'
   },
   {
     date: '2014-04-02',
-    title: 'Sloan funding dat development',
+    title: 'Sloan funding Dat development',
     author: 'Waldo Jacquith',
     teaser: 'Announcing Sloans support of the dat project',
     name: '2014-04-02-dat'
