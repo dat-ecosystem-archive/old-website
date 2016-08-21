e7362de7232c65c46cf51a9837e402ba11993eef
karissa <krmckelv@gmail.com>
2016-08-21 15:58:22 +0200

    get data rendering

--- js/index.js
+++ js/index.js
@@ -1,19 +1,30 @@
 var hyperdrive = require('hyperdrive')
+var pump = require('pump')
 var memdb = require('memdb')
+var hyperquest = require('hyperquest')
 var swarm = require('hyperdrive-archive-swarm')
+var data = require('render-data')
 
 var drive = hyperdrive(memdb())
-var archive = drive.createArchive()
 
-// var ws = archive.createFileWriteStream('hello.txt') // add hello.txt
-//
-// ws.write('hello')
-// ws.write('world')
-// ws.end()
-//
-// var link = archive.key.toString('hex')
-// console.log(link)
+// TODO: make this archive instance a reader instead of a writer
+var archive = drive.createArchive({live: false})
+var filepath = 'static/img/bids.geojson'
+var ws = archive.createFileWriteStream(filepath) // add hello
 
-//document.getElementById('example').innerHTML = link
+pump(hyperquest(window.location.href + filepath), ws, function end (err) {
+  archive.finalize(function () {
+    var link = archive.key.toString('hex')
+    var stream = archive.createFileReadStream(filepath)
+    var file = {
+      name: filepath,
+      createReadStream: function () {
+        return stream
+      }
+    }
+    data.append(file, '#example', function (err) {
+      if (err) throw err
+    })
 
-//swarm(archive)
+  })
+})
